551019782d4e83c85ce6fcd9415086cd4b8ddd02
catherine244 <ccayreen24@gmail.com>
2020-01-22 12:06:57 +0300

    added a function to save images in views.py of posts

--- app/posts/views.py
+++ app/posts/views.py
@@ -14,3 +14,18 @@ from ..main import views
 
 posts = Blueprint('posts', __name__)
 quotes = get_quote()
+
+
+def save_picture(form_image):
+    randome_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_image.filename)
+    picture_name = randome_hex + f_ext
+    picture_path = os.path.join(current_app.root_path, 'static/featured_images', picture_name)
+    
+    output_size = (1000, 400)
+    final_image = Image.open(form_image)
+    final_image.thumbnail(output_size)
+    
+    final_image.save(picture_path)
+    
+    return picture_name
