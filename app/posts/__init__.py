ae84e9f53fa32140a1c0595c064df86a25fbd862
catherine244 <ccayreen24@gmail.com>
2020-01-22 11:30:04 +0300

    added forms in __init__.py of posts

---
+++ app/posts/__init__.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, TextField, SelectField, widgets
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+from flask_login import current_user
