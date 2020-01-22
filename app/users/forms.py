f02917e4baf844c19a0c3c89cd90fafca8723499
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-22 12:45:07 +0300

    Added the class for the fields for the login form

--- app/users/forms.py
+++ app/users/forms.py
@@ -25,3 +25,8 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is taken. Please choose a different one')
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
