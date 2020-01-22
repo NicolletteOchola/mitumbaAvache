bc5c6796faad56aa5ac30dd7b936cf6a4c4d62f8
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-22 10:54:15 +0300

    Added a class for the user and the details they will have on the database

--- app/models.py
+++ app/models.py
@@ -8,3 +8,36 @@ from werkzeug.security import generate_password_hash,check_password_hash
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(225), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique= True, nullable= False, index = True)
+    image = db.Column(db.String(225), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
+    pass_secure = db.Column(db.String(255))
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+     @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+
+    @password.setter
+    def password(self,password):
+            self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
