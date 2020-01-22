a87b4633a34a4533c07b8805aa5269a34236f688
Nicole Ocholla <nicoleochola@gmail.com>
2020-01-22 10:50:31 +0300

    Added all the imports and the login function.

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from app import db, login_manager
+from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
